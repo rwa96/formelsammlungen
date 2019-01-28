234f6df7d499a7f8852d813eb86673cbe9f80133
Robin Wismeth <kontakt@robin-wismeth.de>
2019-01-28 11:28:02 +0100

    final draft

--- vorlesung/old/Übungen/lab1/passwd_printer.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-
-with open("/etc/passwd", "r") as passwd_file:
-	for line in passwd_file.readlines():
-		fields = line.strip("\n").split(":")
-		print(f"{fields[0]} {fields[2]}")
